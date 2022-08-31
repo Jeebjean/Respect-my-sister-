56a54f096c77409edbdbe9f26d1ed380b73272ed
Jeebjean <jeebjean@gmail.com>
2022-08-31 01:26:35 -0500

    update

--- js/index.js
+++ js/index.js
@@ -19,3 +19,18 @@ window.onscroll = () => {
   line1.style.left = `${pos}px`;
   line2.style.right = `${pos}px`;
 };
+
+var acc = document.getElementsByClassName("faq_question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
