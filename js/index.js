bf5f11c7796d4fe202e5e39fc0095572aaff938e
Jeebjean <jeebjean@gmail.com>
2022-09-26 11:13:45 -0500

    update

--- js/index.js
+++ js/index.js
@@ -34,3 +34,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+// profil_but
+
+var acct = document.getElementsByClassName("profil_but");
+var i;
+
+for (i = 0; i < acct.length; i++) {
+  acct[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
